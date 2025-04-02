47c31ff7987cbc20086151e95a121ace04fea4ca
MSI\icewi <icewindflyhero@gmail.com>
2025-04-02 22:51:59 +0800

    7th commit - structure change

--- AutoMapperBuilder/Program.cs
+++ AutoMapperBuilder/Program.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using static AutoMapperBuilder.Mapper;
@@ -13,39 +15,40 @@ namespace AutoMapperBuilder
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
             List<Source> sourceDataList = new List<Source>
-            {
-                new Source
-                {
-                    Name = "John",
-                    idNumber = 1,
-                    Grades = new List<Grade>
-                    {
-                        new Grade { Subject = "Math", score = 100 }
-                    }
-                },
-                new Source
-                {
-                    Name = "Cindy",
-                    idNumber = 2,
-                    Grades = new List<Grade>
-                    {
-                        new Grade { Subject = "Math", score = 96 }
-                    }
-                },
-                new Source
-                {
-                    Name = "Teddy",
-                    idNumber = 3,
-                    Grades = new List<Grade>
-                    {
-                        new Grade { Subject = "Math", score = 99 }
-                    }
-                }
-};
+                        {
+                            new Source
+                            {
+                                Name = "John",
+                                idNumber = 1,
+                                Grades = new List<Grade>
+                                {
+                                    new Grade { Subject = "Math", score = 100 }
+                                }
+                            },
+                            new Source
+                            {
+                                Name = "Cindy",
+                                idNumber = 2,
+                                Grades = new List<Grade>
+                                {
+                                    new Grade { Subject = "Math", score = 96 }
+                                }
+                            },
+                            new Source
+                            {
+                                Name = "Teddy",
+                                idNumber = 3,
+                                Grades = new List<Grade>
+                                {
+                                    new Grade { Subject = "Math", score = 99 }
+                                }
+                            }
+            };
 
 
-            var mappedList = (List<Destination>)Map<Destination, Source>(sourceDataList);
+            var mappedList = ObjectMapper.Map<Destination, Source>(sourceDataList);
 
             //IEnumerable<Destination> DestinationList = Mapper.Map<Destination, Source>(sourceDataList);
 
@@ -62,6 +65,84 @@ namespace AutoMapperBuilder
             }
 
             Console.WriteLine(mappedList);
+
+
+            //Student student = new Student()
+            //{
+            //   Name = "Leo123",
+            //   SignIn = 6,
+            //   Chinese = 80,
+            //   English = 60,
+            //   Math = 20
+
+            //};
+
+
+            //Student student2 = new Student()
+            //{
+            //    Name = "Kelly",
+            //    SignIn = 9,
+            //    Chinese = 0,
+            //    English = 10,
+            //    Math = 95
+
+            //};
+
+
+            //Student student3 = new Student()
+            //{
+            //    Name = "Bob",
+            //    SignIn = 2,
+            //    Chinese = 20,
+            //    English = 100,
+            //    Math = 70
+            //};
+
+            // 委派→一種策略模式
+            // Mapper.Map<User>(student,x=>x.Email,x=>x.EmailAddress)
+            //ShowFinalScore(student, x=> x.SignIn,x=> x.Chinese);
+            //ShowFinalScore(student2, x => x.SignIn, x => x.Math);
+            //ShowFinalScore(student3, x => x.SignIn, x => x.English);
+
+
         }
+
+
+        //static double AdjustScore(int condition,double score)
+        //{
+        //    if(condition > 8)
+        //    {
+        //       return score *= 1.1;
+        //    }
+        //    else if(condition > 5)
+        //    {
+        //        return score *= 1.05;
+        //    }
+        //    else
+        //    {
+        //        return score;
+        //    }
+        //}
+
+        // expression在這邊可解決欄位不對稱的問題
+        //static void ShowFinalScore(Student student,Expression<Func<Student,int>> condition,Expression<Func<Student,double>> score)
+        //{
+        //    MemberExpression memberExpression = condition.Body as MemberExpression;
+        //    if(memberExpression.Member is PropertyInfo property) // 轉成property類型再去拿
+        //    {
+        //      int value = (int)property.GetValue(student);
+        //    }
+        //    else if (memberExpression.Member is FieldInfo fieldInfo)
+        //    {
+
+        //    }
+
+
+        //    //int a = condition.Compile().Invoke(student);
+        //    //double b = score.Compile().Invoke(student);
+        //    //double finalScore = AdjustScore(a,b);
+
+        //    //Console.WriteLine($"姓名:{student.Name} 期末成績:{finalScore}");
+        //}
     }
 }
---
+++ AutoMapperBuilder/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMapperBuilder
+{
+    internal class Student
+    {
+        public String Name { get; set; }    
+        public int SignIn { get; set; }
+        public double Chinese {  get; set; }     
+        public double English {  get; set; }     
+        public double Math {  get; set; }     
+    }
+}
